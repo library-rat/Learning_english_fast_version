d299b9c4c6b91f9474b43a45964071d692956f5d
library-rat <1ucas_z@protonmail.com>
2024-04-06 13:08:03 +0200

    init save when first launch

--- home_screen.gd
+++ home_screen.gd
@@ -3,9 +3,28 @@ extends Control
 var json_as_text = FileAccess.get_file_as_string("res://assets/text_Jsonified.json")
 var json_dict = JSON.parse_string(json_as_text)
 var lvlselector = load("res://level_selector.tscn")
+var currentlvl = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for key in json_dict.keys() :
 		var newLvl = lvlselector.instantiate()
 		newLvl.set_value(key)
 		$SelectorContainer.add_child(newLvl) 
+	load_currentlvl()
+
+
+func load_currentlvl():
+	if not FileAccess.file_exists("user://savegame.save"):
+		
+		for keys in json_dict.keys() :
+			currentlvl[keys] = 0
+		save_progression()
+		return
+	else :
+		var save_game = FileAccess.open("user://savegame.save", FileAccess.READ)
+	
+	
+func save_progression():
+	var savegame = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var save_string = JSON.stringify(currentlvl)
+	savegame.store_line(save_string)
